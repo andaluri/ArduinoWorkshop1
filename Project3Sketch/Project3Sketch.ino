99d1daa3191ae9669d5eda49eed16aa998e5f8a2
Sambasiva Andaluri <samrao@andaluri.com>
2014-07-04 17:23:14 -0400

    Initial commit

---
+++ Project3Sketch/Project3Sketch.ino
@@ -0,0 +1,41 @@
+
+const int  buttonPin = 2;    // the pin that the pushbutton is attached to
+
+int buttonState = 0;         // current state of the button
+int lastButtonState = 0;     // previous state of the button
+
+void setup() {
+  // initialize the button pin as a input:
+  pinMode(buttonPin, INPUT);
+
+  // initialize serial communication:
+  Serial.begin(9600);
+}
+
+void loop() {
+  
+  // read the pushbutton input pin:
+  buttonState = digitalRead(buttonPin);
+
+  // compare the buttonState to its previous state
+  if (buttonState != lastButtonState) {
+    
+    // if the state has changed, increment the counter
+    if (buttonState == HIGH) {
+      
+      // if the current state is HIGH then the button
+      // wend from off to on:      
+      Serial.println("Button is ON");
+    } else {
+      
+      // if the current state is LOW then the button
+      // went from on to off:
+      Serial.println("Button is OFF"); 
+    }
+  
+  }
+  
+  // save the current state as the last state, 
+  //for next time through the loop
+  lastButtonState = buttonState;    
+}
